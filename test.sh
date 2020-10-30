0f755e8b8cacc1a2f0333403846b862e4bda3e22
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2020-10-31 04:11:39 +0530

    Added uvicorn to requirements.txt

--- test.sh
+++ test.sh
@@ -3,8 +3,6 @@ killall -9 redis-server
 killall -9 uvicorn
 killall -9 python3
 
-sudo apt-get update
-sudo apt-get install uvicorn
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
