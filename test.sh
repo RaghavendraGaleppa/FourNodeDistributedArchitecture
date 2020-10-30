e0a73b7fdb0fba578542bea766e5cf0635ab94e5
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2020-10-31 04:07:37 +0530

    added new rules

--- test.sh
+++ test.sh
@@ -3,13 +3,10 @@ killall -9 redis-server
 killall -9 uvicorn
 killall -9 python3
 
+sudo apt-get update
 sudo apt-get install uvicorn
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
-make
-make install
-
-redis-server 
-uvicorn server:app --reload
-python3 ./verify_payload.py
+sudo make > /dev/null
+sudo make install > /dev/null
