f8052c91553fd01d83f2f8bc8edf8fbbed816c6a
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2020-10-31 03:53:28 +0530

    updated test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,13 @@ killall -9 redis-server
 killall -9 uvicorn
 killall -9 python3
 
+sudo apt-get install uvicorn
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+make install
+
 redis-server 
 uvicorn server:app --reload
 python3 verify_payload.py
